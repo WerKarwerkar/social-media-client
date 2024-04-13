b554d5e80555884915fca7be9e5848343b49b0eb
WerKarwerkar <126430261+WerKarwerkar@users.noreply.github.com>
2024-04-13 23:59:11 +0200

    update

--- cypress/testing/button.cy.js
+++ cypress/testing/button.cy.js
@@ -1,28 +1,20 @@
 describe("Logout", () => {
   it("Successfully logs in and then logs out", () => {
-    // Visit the page
-    cy.visit("https://weronikamartinsen.github.io/social-media-client/");
+    cy.visit("/");
 
-    // Click the login button to open the login modal
     cy.get('button[data-auth="login"]').click();
 
-    // Wait for the modal to become visible
     cy.wait(500);
 
-    // Type in the email and password
     cy.get("#loginEmail").type("stefaF1@noroff.no");
     cy.get("#loginPassword").type("stefaFirst1");
 
-    // Submit the login form
     cy.get("#loginForm").find("button").contains("Login").click();
 
-    // Wait for the login to be processed
     cy.wait(1000);
 
-    // Click the logout button
     cy.get('button[data-auth="logout"]').click();
 
-    // Check if the login button is visible again after the user has been logged out
     cy.get('button[data-auth="login"]').should("be.visible");
   });
 });
--- src/js/api/auth/logout.test.js
+++ src/js/api/auth/logout.test.js
@@ -1,26 +1,21 @@
 import { logout } from "./logout";
 import * as storageModule from "../../storage/index";
 
-// Mock the remove function from the storage module
 jest.mock("../../storage/index", () => ({
   remove: jest.fn(),
 }));
 
 describe("Testing logout functionality", () => {
-  // Reset mocks before each test to ensure a clean environment
   beforeEach(() => {
     jest.resetAllMocks();
   });
 
   it("should clear the user's token and profile from storage", () => {
-    // Execute the logout function
     logout();
 
-    // Verify that the remove function from the storage module was called for 'token' and 'profile'
     expect(storageModule.remove).toHaveBeenCalledWith("token");
     expect(storageModule.remove).toHaveBeenCalledWith("profile");
 
-    // Additionally, check that the remove function was called exactly two times
     expect(storageModule.remove).toHaveBeenCalledTimes(2);
   });
 });
