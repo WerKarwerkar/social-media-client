932158ffc66850f00205931ff24f72686245ca49
WerKarwerkar <126430261+WerKarwerkar@users.noreply.github.com>
2024-04-14 15:08:33 +0200

    suffcessss

--- cypress/e2e/button.cy.js
+++ cypress/e2e/button.cy.js
@@ -1,20 +1,22 @@
-describe("Logout", () => {
+describe("Login and Logout flow", () => {
   it("Successfully logs in and then logs out", () => {
-    cy.visit("/");
-    cy.wait(1000);
+    cy.visit("https://werkarwerkar.github.io/social-media-client/");
 
-    cy.get('button[data-auth="login"]').click();
-    cy.wait(500);
+    cy.get('button[data-auth="login"]').first().click();
 
     cy.get("#loginEmail").type("stefaF1@noroff.no");
+
     cy.get("#loginPassword").type("stefaFirst1");
-    cy.get("#loginForm").find("button").contains("Login").click();
 
-    cy.wait(1000);
+    cy.get("#loginForm button").contains("Login").click();
 
-    cy.get('button[data-auth="logout"]')
-      .contains("Logout")
-      .click({ multiple: true });
-    cy.get('button[data-auth="login"]').should("be.visible");
+    cy.get("button").contains("Logout").should("be.visible");
+
+    cy.get('button[data-auth="logout"]').should("be.visible");
+    cy.get('button[data-auth="logout"]').click();
+
+    cy.get(
+      'button[data-bs-toggle="modal"][data-bs-target="#loginModal"]',
+    ).should("be.visible");
   });
 });
