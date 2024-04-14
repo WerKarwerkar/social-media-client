a0b083e2cf05508bbbf59a97026a7171999a7196
WerKarwerkar <126430261+WerKarwerkar@users.noreply.github.com>
2024-04-14 13:18:20 +0200

    update

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,19 @@
 describe("Login Form", () => {
   it("User can login with valid credentials", () => {
     cy.visit("https://werkarwerkar.github.io/social-media-client/");
-    cy.get('button[data-auth="login"]').first().click();
+
+    cy.wait(1000);
+
+    cy.get('button[data-auth="login"][data-bs-toggle="modal"]').click();
+
     cy.get("#loginEmail").type("stefaF1@noroff.no");
     cy.get("#loginPassword").type("stefaFirst1");
+
+    wait(1000);
     cy.get("#loginForm button").contains("Login").click();
+
+    cy.wait(1000);
+
     cy.get("button").contains("Logout").should("be.visible");
   });
 });
