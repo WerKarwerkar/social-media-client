e5e068ac99e378e9a5fd6fcfc050e1fdcc73949e
WerKarwerkar <126430261+WerKarwerkar@users.noreply.github.com>
2024-04-14 13:07:47 +0200

    add url

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,6 @@
 describe("Login Form", () => {
   it("User can login with valid credentials", () => {
-    cy.visit("/");
+    cy.visit("https://werkarwerkar.github.io/social-media-client/");
     cy.get('button[data-auth="login"]').first().click();
     cy.get("#loginEmail").type("stefaF1@noroff.no");
     cy.get("#loginPassword").type("stefaFirst1");
